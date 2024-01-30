b2a30b0f1d24824f0a619d9d89edb1649aba060a
vishal-joshy <1812-vishal@protonmail.com>
2024-01-30 11:19:13 +0530

    Add absolute imports

--- template/vite.config.ts
+++ template/vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@":path.resolve(__dirname,"./src"),
+    }
+  },
   plugins: [react()],
+  define: {
+    "process.env" : process.env
+  },
+  build:{
+    outDir: 'build',
+    target: 'esnext'
+  }
 })

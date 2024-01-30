7f81af09bda07bcfe8a051dd95fea55bfe78db64
vishal-joshy <1812-vishal@protonmail.com>
2024-01-30 12:55:46 +0530

    Add prettier dependency

--- template/vite.config.ts
+++ template/vite.config.ts
@@ -1,20 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from "path"
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
-      "@":path.resolve(__dirname,"./src"),
-    }
+      '@': path.resolve(__dirname, './src'),
+    },
   },
   plugins: [react()],
   define: {
-    "process.env" : process.env
+    'process.env': process.env,
   },
-  build:{
+  build: {
     outDir: 'build',
-    target: 'esnext'
-  }
-})
+    target: 'esnext',
+  },
+});

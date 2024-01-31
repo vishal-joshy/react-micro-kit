437fee534ef9c276be06cbf2fcaf47249e7b9400
vishal-joshy <1812-vishal@protonmail.com>
2024-01-31 11:04:03 +0530

    Add sample component

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -1,5 +1,9 @@
-import React from 'react';
+import SampleComponent from '@/components/Example';
 
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world</h1>;
+  return (
+    <h1 className="text-3xl font-bold underline">
+      <SampleComponent />
+    </h1>
+  );
 }
